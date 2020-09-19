635a7a299c395a58b268694a89c72454300175a0
SantiNotSancor <santiagocorvalan6@gmail.com>
2020-09-18 21:34:41 -0300

    Proyecto en Blanco

---
+++ ProyectoLaberinto/Source/ProyectoLaberinto/ProyectoLaberintoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProyectoLaberintoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROYECTOLABERINTO_API AProyectoLaberintoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
